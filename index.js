e759bbc7319d75305831d26f90a39fd18b40f66a
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-02 21:26:00 +1000

    Test passed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+  name: 'Siri',
+  streetAddress: '1707/850 Whitehorse Road'
+}
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+  const newEmployee = {...employee};
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key) {
+  const newDeleteEmployee = {...employee};
+  delete newDeleteEmployee[key];
+  return newDeleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+  delete employee[key];
+  return employee;
+}
